8ff3b23b5fb78d039d9b8cacd28d9f4dc81d65f8
Philip Nelson <philipnelson5@gmail.com>
2021-12-03 20:06:04 -0700

    consolidate days

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -7,18 +7,6 @@ use std::fs::File;
 use std::path::Path;
 use std::iter::zip;
 
-fn solve_1(vals: Vec<u32>) -> u32 {
-    let mut count = 0;
-    let mut last = u32::MAX;
-    for next in vals {
-        if next > last {
-            count += 1;
-        }
-        last = next;
-    }
-    return count;
-}
-
 fn get_input(filename: impl AsRef<Path>) -> Vec<u32> {
     let f = File::open(filename).expect("unable to open file");
     let f = BufReader::new(f);
@@ -28,13 +16,27 @@ fn get_input(filename: impl AsRef<Path>) -> Vec<u32> {
         .collect();
 }
 
-fn main() {
-
-    let input = get_input("input.txt");
+fn problem_1(input: &Vec<u32>) {
     let mut it = input.iter();
     it.advance_by(1);
     let count = zip(input.iter(), it)
         .rfold(0, |acc, (a, b)| if a < b { acc + 1 } else { acc });
 
     println!("{}", count);
+}
+
+fn problem_2(input: &Vec<u32>) {
+    let chunks: Vec<u32> = input.windows(3).map(|x| x.iter().sum()).collect();
+    let mut it = chunks.iter();
+    it.advance_by(1);
+    let count = zip(chunks.iter(), it)
+        .rfold(0, |acc, (a, b)| if a < b { acc + 1 } else { acc });
+
+    println!("{}", count);
+}
+
+fn main() {
+    let input = get_input("input.txt");
+    problem_1(&input);
+    problem_2(&input);
 }
--- 2021/day-02/src/main.rs
+++ 2021/day-02/src/main.rs
@@ -15,15 +15,34 @@ fn parse(line: &String) -> [i32; 2] {
     let val = vals[1].parse::<i32>().expect("unable to parse");
     match command.as_ref() {
         "forward" => [0, val],
-        "up" => [-val, 0],
-        "down" => [val, 0],
-        _ => [0, 0]
+        "up"      => [-val, 0],
+        "down"    => [val, 0],
+        _         => [0, 0]
     }
 }
 
-fn main() {
-    let answer = get_input("input.txt").iter()
+fn problem_1(input: &Vec<String>) {
+    let answer = input.iter()
         .map(parse)
         .fold([0,0], |acc, curr| [acc[0] + curr[0], acc[1] + curr[1]]);
     println!("{}", answer[0] * answer[1]);
+}
+
+fn problem_2(input: &Vec<String>) {
+    // [ depth , horizonal , aim ]
+    let answer = input.iter()
+        .map(parse)
+        .fold([0,0,0], |acc, curr|
+            if curr[1] != 0 {
+                [acc[0] + acc[2] * curr[1], acc[1] + curr[1], acc[2]]
+            } else {
+                [acc[0], acc[1], acc[2] + curr[0]]
+            });
+    println!("{}", answer[0] * answer[1]);
+}
+
+fn main() {
+    let input = get_input("input.txt");
+    problem_1(&input);
+    problem_2(&input);
 }
